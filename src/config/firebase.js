fd9876de9d8b3156c26a7c5542de3ed4cb576f61
anhphambk22 <hoangenglish2401@gmail.com>
2026-01-27 23:57:12 +0700

    fix: use Render Secret File for Firebase service account

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,23 @@
-
 import admin from "firebase-admin";
 import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
-);
+let serviceAccount;
+if (process.env.RENDER) {
+  // Render: đọc từ Secret File
+  serviceAccount = JSON.parse(
+    fs.readFileSync("/etc/secrets/FIREBASE_SERVICE_ACCOUNT", "utf8")
+  );
+} else {
+  // Local: đọc từ file trong src/config
+  serviceAccount = JSON.parse(
+    fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
+  );
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://bk-iot-26-default-rtdb.asia-southeast1.firebasedatabase.app"
+  databaseURL: "https://bk-iot-26-default-rtdb.asia-southeast1.firebasedatabase.app",
 });
 
 export const db = admin.database();
+export default admin;
